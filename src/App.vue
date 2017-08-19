bfd07aecaa27c7ec4ed9f39540e468cc9d5d803c
John V Runion <jvrunion@gmail.com>
2017-08-19 13:59:04 -0400

    added in sticky nav from uikit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template lang="pug">
 #app
-  img(src='./assets/logo.png')
-  h1 {{ msg }}
-  h2 Essentials
-  ul
-    li
-      a(href='https://vuejs.org', target='_blank') Core Docs
-    li
-      a(href='https://forum.vuejs.org', target='_blank') Forum
-    li
-      a(href='https://gitter.im/vuejs/vue', target='_blank') Gitter Chat
-    li
-      a(href='https://twitter.com/vuejs', target='_blank') Twitter
-  h2 The Vue Ecosystem
-  ul
-    li
-      a(href='http://router.vuejs.org/', target='_blank') vue-router
-    li
-      a(href='http://vuex.vuejs.org/', target='_blank') vuex
-    li
-      a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
-    li
-      a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
+  .uk-container-expand
+    div(uk-sticky='sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar')
+      nav.uk-navbar-container(uk-navbar='', style='position: relative; z-index: 980;')
+        .uk-navbar-left
+          ul.uk-navbar-nav
+            li.uk-active
+              a(href='#') Active
+            li
+              a(href='#') Parent
+              .uk-navbar-dropdown
+                ul.uk-nav.uk-navbar-dropdown-nav
+                  li.uk-active
+                    a(href='#') Active
+                  li
+                    a(href='#') Item
+                  li
+                    a(href='#') Item
+            li
+              a(href='#') Item
+    img(src='./assets/logo.png')
+    h1 {{ msg }}
+    h2 Essentials
+    ul
+      li
+        a(href='https://vuejs.org', target='_blank') Core Docs
+      li
+        a(href='https://forum.vuejs.org', target='_blank') Forum
+      li
+        a(href='https://gitter.im/vuejs/vue', target='_blank') Gitter Chat
+      li
+        a(href='https://twitter.com/vuejs', target='_blank') Twitter
+    h2 The Vue Ecosystem
+    ul
+      li
+        a(href='http://router.vuejs.org/', target='_blank') vue-router
+      li
+        a(href='http://vuex.vuejs.org/', target='_blank') vuex
+      li
+        a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
+      li
+        a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
 </template>
 
 <script>
@@ -47,7 +66,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 h1, h2 {
