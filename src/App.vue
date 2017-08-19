6433eab70286f4081dfc759b2977498ddc780e25
John V Runion <jvrunion@gmail.com>
2017-08-19 14:25:04 -0400

    adding some basic style treatments

--- src/App.vue
+++ src/App.vue
@@ -18,29 +18,32 @@
                   li
                     a(href='#') Item
             li
-              a(href='#') Item
-    img(src='./assets/logo.png')
-    h1 {{ msg }}
-    h2 Essentials
-    ul
-      li
-        a(href='https://vuejs.org', target='_blank') Core Docs
-      li
-        a(href='https://forum.vuejs.org', target='_blank') Forum
-      li
-        a(href='https://gitter.im/vuejs/vue', target='_blank') Gitter Chat
-      li
-        a(href='https://twitter.com/vuejs', target='_blank') Twitter
-    h2 The Vue Ecosystem
-    ul
-      li
-        a(href='http://router.vuejs.org/', target='_blank') vue-router
-      li
-        a(href='http://vuex.vuejs.org/', target='_blank') vuex
-      li
-        a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
-      li
-        a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
+              a(href='#') Item          
+  .uk-container-expand
+    .uk-grid.uk-flex-center.uk-text-center.widget-main
+      .uk-card.uk-card-primary.uk-card-body.widget-content
+        h1 {{ msg }}
+        h2 Essentials
+        ul
+          li
+            a(href='https://vuejs.org', target='_blank') Core Docs
+          li
+            a(href='https://forum.vuejs.org', target='_blank') Forum
+          li
+            a(href='https://gitter.im/vuejs/vue', target='_blank') Gitter Chat
+          li
+            a(href='https://twitter.com/vuejs', target='_blank') Twitter                  
+        img(src='./assets/logo.png')
+        h2 The Vue Ecosystem
+        ul
+          li
+            a(href='http://router.vuejs.org/', target='_blank') vue-router
+          li
+            a(href='http://vuex.vuejs.org/', target='_blank') vuex
+          li
+            a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
+          li
+            a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
 </template>
 
 <script>
@@ -66,24 +69,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+  .widget-main {
+    height: 100vh;
+    .widget-content {
+      h1, h2 {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        font-weight: normal;
+        color: #fff;
+      }
 
-h1, h2 {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  font-weight: normal;
-}
+      ul {
+        list-style-type: none;
+        padding: 0;
+      }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+      li {
+        display: inline-block;
+        margin: 0 10px;
+      }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+      a {
+        color: #fff;
+      }  
+    }  
+  }    
 }
 </style>
